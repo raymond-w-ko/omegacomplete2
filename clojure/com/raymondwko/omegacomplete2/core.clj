35467618d7714897af57d04335d59477874867ef
Raymond W. Ko <raymond.w.ko@gmail.com>
2013-04-03 19:40:48 -0400

    use print for now as it is safer

--- clojure/com/raymondwko/omegacomplete2/core.clj
+++ clojure/com/raymondwko/omegacomplete2/core.clj
@@ -8,5 +8,5 @@
 (defn capture-buffer [buffer_id]
   (let [buf (Vim/buffer (str buffer_id))
         lines (map #(.getLine buf %) (range 1 (+ 1 (.getNumLines buf))))]
-    (Vim/msg (reduce + (map #(count %) lines)))
+    (Vim/print (reduce + (map #(count %) lines)))
     ))
