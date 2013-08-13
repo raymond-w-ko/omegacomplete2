2a14616f792b35138fa2e89035fc78898f2d3f70
Raymond W. Ko <raymond.w.ko@gmail.com>
2013-08-12 23:03:00 -0400

    some more progress, copy and paste from omegacomplete1

--- clojure/com/raymondwko/omegacomplete2/core.clj
+++ clojure/com/raymondwko/omegacomplete2/core.clj
@@ -2,13 +2,13 @@
   [:import [vim Vim Clojure] [java.io StringWriter]]
   [:use [clojure.pprint :only (pprint)]])
 
-(def work-queue nil)
-(def buffer-to-word-count nil)
+(def buffer-snapshot-queue nil)
+(def buffer-to-word-with-count nil)
 (def global-word-count nil)
 
 (defn trim-hyphen-prefix-suffix
-  "trim leading and trailing hyphens on words as a result of increment and
-   decrement operators in C style languages"
+  "trim leading and trailing hyphens on words that could have resulted from pre
+   or post decrement in a C style language"
   [word]
   (-> word
       (clojure.string/replace #"^-+?" "")
@@ -17,68 +17,97 @@
 (defn debug-write [thing]
   (spit "C:\\omegacomplete2.txt" (pr-str thing)))
 
-(defn offer-job! [job] (.offerLast work-queue job))
-(defn take-job! [] (.takeFirst work-queue))
+(defn offer-job! [job] (.offerLast buffer-snapshot-queue job))
+(defn take-job! [] (.takeFirst buffer-snapshot-queue))
+
+(def banned-buffers
+  #("GoToFile" "ControlP" "__Scratch__" "__Gundo__" "__Gundo_Preview__"))
 
 (defn capture-buffer
-  "captures a buffer and queues it for processing by the background thread"
+  "creates a snapshot of a buffer and queues it for processing by the
+   background thread"
   [buffer-id]
-  (let [buf (Vim/buffer (str buffer-id))
-        lines (map #(.getLine buf %) (range 1 (+ 1 (.getNumLines buf))))]
+  (let [buffer (Vim/buffer (str buffer-id))
+        buffer-name (.getName buffer)
+        line-numbers (range 1 (+ 1 (.getNumLines buffer)))
+        lines (map #(.getLine buffer %) line-numbers)]
     (offer-job! (list buffer-id lines))
     (debug-write @global-word-count)
     ))
 
-(defn get-count [coll k]
+(defn get-count
+  "gets the value of collection coll given the key k, but returns 0 if the key
+   doesn't exist"
+  [coll k]
   (let [v (coll k)]
     (if (nil? v) 0 v)))
 
-(defn make-word-count [word-list]
+(defn make-word-count-map
+  "transforms a list of words into a mapping of word to number of occurrence"
+  [word-list]
   (reduce (fn [coll k] (assoc coll k (+ 1 (get-count coll k))))
           {} word-list))
 
-(defn update-global-word-count [old-global word-count op]
+(defn update-global-word-count
+  "updates the mapping of words to reference count"
+  [old-global-word-count word-count op]
   (letfn [(func [coll keyval]
             (let [word (key keyval)
                   delta-count (val keyval)
-                  current-count (get-count coll word)
-                  total-count (op current-count delta-count)]
-              (if (= 0 total-count)
+                  prev-count (get-count coll word)
+                  new-count (op prev-count delta-count)]
+              (if (= 0 new-count)
                 (dissoc coll word)
-                (assoc coll word total-count))))]
-    (reduce func old-global word-count)))
+                (assoc coll word new-count))))]
+    (reduce func old-global-word-count word-count)))
 
-(defn process-buffer-snapshot [old-map buffer-id buffer-lines]
-  (let [old-word-count
-        (old-map buffer-id) 
+(defn process-buffer-snapshot [old-buffer-to-word-with-count id lines]
+  (let [old-word-count (old-buffer-to-word-with-count id) 
         ,
         word-list
-        (->> buffer-lines
+        (->> lines
              (map #(clojure.string/split % #"[^a-zA-Z0-9\-]+?"))
              (map #(map trim-hyphen-prefix-suffix %))
              (map #(filter (fn [word] (not-empty word)) %))
              (map #(concat %))
              (reduce concat))
         ,
-        word-count
-        (make-word-count word-list)]
+        word-count (make-word-count-map word-list)]
     (swap! global-word-count update-global-word-count word-count +)
     (if old-word-count
       (swap! global-word-count update-global-word-count old-word-count -))
-    (assoc old-map buffer-id word-count)))
+    (assoc old-buffer-to-word-with-count id word-count)))
 
-(defn background-worker []
+(defn background-worker
+  "consumes buffer snapshots from buffer-snapshot-queue and updates
+   global-word-count and buffer-to-word-with-count"
+  []
   (let [job (take-job!)
         [buffer-id buffer-lines] job]
-    (swap! buffer-to-word-count process-buffer-snapshot buffer-id buffer-lines)) 
+    (swap! buffer-to-word-with-count
+           process-buffer-snapshot buffer-id buffer-lines)) 
   (recur))
+
+(defn calculate-and-fill-results
+  []
+  (let [results (Vim/eval "g:omegacomplete2_results")]
+    (.add results "cheeseburger") 
+    (.add results "meow") 
+    (.add results "purr") 
+    "undefined"))
+
+(defn set-is-corrections-only
+  []
+  (if false
+    (Vim/command "let is_corrections_only=1")
+    (Vim/command "let is_corrections_only=0")))
 
 (defn init []
   ; uncomment this to have Vim print normal REPL output
   ;(set! (. Clojure PRINT_REPL_OUTPUT) true)
 
-  (def work-queue (java.util.concurrent.LinkedBlockingDeque.))
-  (def buffer-to-word-count (atom {}))
+  (def buffer-snapshot-queue (java.util.concurrent.LinkedBlockingDeque.))
+  (def buffer-to-word-with-count (atom {}))
   (def global-word-count (atom (sorted-map)))
 
   (doto
--- plugin/omegacomplete2.vim
+++ plugin/omegacomplete2.vim
@@ -7,9 +7,28 @@ if !has('java')
     finish
 end
 
+if !exists("g:omegacomplete2_normal_hi_cmds")
+    let g:omegacomplete2_normal_hi_cmds=[
+    \ "hi Pmenu guifg=#00ff00 guibg=#003300 gui=none " .
+             \ "ctermbg=022 ctermfg=046 cterm=none",
+    \ "hi PmenuSel guifg=#003300 guibg=#00ff00 gui=none " .
+                \ "ctermbg=046 ctermfg=022 cterm=none",
+        \ ]
+endif
+if !exists("g:omegacomplete2_corrections_hi_cmd")
+    let g:omegacomplete2_corrections_hi_cmd=[
+    \ "hi Pmenu guifg=#ffff00 guibg=#333300 gui=none " .
+              \"ctermbg=058 ctermfg=011 cterm=none",
+    \ "hi PmenuSel guifg=#333300 guibg=#ffff00 gui=none " .
+                \ "ctermbg=011 ctermfg=058 cterm=none",
+        \ ]
+endif
+
 let s:omegacomplete2_path = ''
+let s:current_hilight = 'undefined'
 
 function s:Init()
+    " find where clojure code exists
     let path_list = split(&runtimepath, ',')
     for path in path_list
         let candidate_path = path . '/clojure/com/raymondwko/omegacomplete2'
@@ -22,6 +41,11 @@ function s:Init()
         echoerr 'could not find omegacomplete2 Clojure directory'
         return
     end
+
+    set completefunc=Omegacomplete2Func
+
+    set completeopt+=menu
+    set completeopt+=menuone
 
     call s:LoadMappings()
 
@@ -42,16 +66,110 @@ endfunction
 
 function s:LoadMappings()
     command Omegacomplete2Reload :call <SID>ReloadClojure()
+
+    nnoremap <silent> i i<C-r>=omegacomplete2#OnInsertModeEvent()<CR>
+    nnoremap <silent> I I<C-r>=omegacomplete2#OnInsertModeEvent()<CR>
+    nnoremap <silent> a a<C-r>=omegacomplete2#OnInsertModeEvent()<CR>
+    nnoremap <silent> A A<C-r>=omegacomplete2#OnInsertModeEvent()<CR>
+    nnoremap <silent> R R<C-r>=omegacomplete2#OnInsertModeEvent()<CR>
+
+    let s:keys_mapping_driven =
+        \ [
+        \ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        \ 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        \ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        \ 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        \ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        \ '-', '_', '<Space>', '<C-h>', '<BS>', 
+        \ ]
+
+        "'~', '^', '.', ',', ':', '!', '#', '=', '%', '$', '@', '<', '>', '/',
+        "'\'
+ 
+    for key in s:keys_mapping_driven
+        exe printf('inoremap <silent> %s %s<C-r>=omegacomplete2#OnInsertModeEvent()<CR>',
+                 \ key, key)
+    endfor
 endfunction
 
-function omegacomplete2#OnBufferInsertModeChange()
+function omegacomplete2#OnInsertModeEvent()
+    " disable when paste mode is active
+    if &paste
+        return ''
+    endif
+
+    javarepl clojure
+
+    call <SID>SyncCurrentBuffer()
+
+    let g:omegacomplete2_results=[]
+
+    exe 'java (com.raymondwko.omegacomplete2/calculate-and-fill-results)'
+
+    if len(g:omegacomplete2_results) == 0
+        " try to show popup menu, but fail and reset completion status
+        return "\<C-x>\<C-u>"
+    else
+        exe 'java (com.raymondwko.omegacomplete2/set-is-corrections-only)'
+        if is_corrections_only && s:current_hilight != 'corrections'
+            for cmd in g:omegacomplete2_corrections_hi_cmd
+                exe cmd
+            endfor
+            let s:current_hilight = 'corrections'
+        elseif !is_corrections_only && s:current_hilight != 'normal'
+            for cmd in g:omegacomplete2_normal_hi_cmds
+                exe cmd
+            endfor
+            let s:current_hilight = 'normal'
+        endif
+        " show actual popup
+        return "\<C-x>\<C-u>\<C-p>"
+    endif
+endfunction
+
+function <SID>SyncCurrentBuffer()
     javarepl clojure
     exe 'java (com.raymondwko.omegacomplete2/capture-buffer ' . bufnr('%') . ')'
 endfunction
 
 augroup omegacomplete2
     autocmd!
-    autocmd CursorMovedI * call omegacomplete2#OnBufferInsertModeChange()
+    autocmd BufLeave * call <SID>SyncCurrentBuffer()
+    autocmd InsertEnter * call <SID>SyncCurrentBuffer()
+    autocmd BufReadPost * call <SID>SyncCurrentBuffer()
 augroup END
+
+function Omegacomplete2Func(findstart, base)
+    if a:findstart
+        if len(g:omegacomplete2_results) == 0
+            if (v:version > 702)
+                return -3
+            else
+                return -1
+            endif
+        endif
+
+        let index = col('.') - 2
+        let line = getline('.')
+        while 1
+            if index == -1
+                break
+            endif
+            if match(line[index], '[a-zA-Z0-9_\-]') == -1
+                break
+            endif
+
+            let index = index - 1
+        endwhile
+        let result = index + 1
+        return result
+    else
+        if (v:version > 702)
+            return {'words' : g:omegacomplete2_results}
+        else
+            return g:omegacomplete2_results
+        endif
+    endif
+endfunction
 
 call s:Init()
